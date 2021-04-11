80c02f491b4412b94f6526e3ec9df41c67578d21
darlaevans2000 <darlaevans2000@gmail.com>
2021-04-11 15:22:04 -0600

    Add data.js with meal options and make random meal function

---
+++ main.js
@@ -0,0 +1,39 @@
+var sideRadio = document.getElementById('side');
+var mainRadio = document.getElementById('main-dish');
+var dessertRadio = document.getElementById('dessert');
+var entireMealRadio = document.getElementById('entire-meal');
+
+var letsCookBtn = document.getElementById('lets-cook');
+var clearBtn= document.querySelector(".clear");
+
+var potImg = document.getElementById('pot-image');
+var selectedSection = document.querySelector('.selected-meal');
+var selectedString = document.getElementById("selected-meal-choice");
+
+letsCookBtn.addEventListener("click", showMeal);
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function togglePotImg() {
+  selectedSection.hidden = false;
+  potImg.hidden = true;
+  clearBtn.hidden= false;
+}
+
+function showMeal() {
+  if (sideRadio.checked)  {
+    selectedString.innerText = `${sides[getRandomIndex(sides)]}!`
+    togglePotImg()
+  } else if (mainRadio.checked) {
+    selectedString.innerText =`${mains[getRandomIndex(mains)]}!`
+    togglePotImg()
+  } else if (dessertRadio.checked) {
+    selectedString.innerText =`${desserts[getRandomIndex(desserts)]}!`
+    togglePotImg()
+  } else if (entireMealRadio.checked) {
+    selectedString.innerText =`${mains[getRandomIndex(mains)]} with a side of ${sides[getRandomIndex(sides)]} and ${desserts[getRandomIndex(desserts)]} for dessert!`
+    togglePotImg()
+  }
+}
